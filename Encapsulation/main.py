d10e7ca9eb755239defb09208394ffdb7a5600b8
Krishnammraju3369 <Krishnam_raju1@outlook.com>
2025-06-24 15:41:00 +0100

    Add files via upload

---
+++ Encapsulation/main.py
@@ -0,0 +1,26 @@
+class BankAccount:
+
+    def __init__(self):
+        self._balance = 0.0
+
+    @property
+    def balance(self):
+        return self._balance
+
+    def deposit(self, amount):
+        if amount <= 0:
+            raise ValueError("Please entre a Valid amount")
+        else:
+            self._balance += amount
+
+    def withdraw(self, amount):
+        if amount >= self._balance:
+            return ValueError("You don't have enough money left in your account")
+        elif amount <= 0:
+            raise ValueError("Withdraw amount must be postive")
+        else:
+            self._balance -= amount
+
+
+user1 = BankAccount()
+print(user1.balance)
